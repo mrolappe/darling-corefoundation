c5050324ce9d240f32e9657142676715e136a54b
Lubos Dolezel <lubos@dolezel.info>
2015-11-13 14:10:59 +0100

    Darling build now working

--- Headers/GNUstepBase/GSConfig.h
+++ Headers/GNUstepBase/GSConfig.h
@@ -1,4 +1,6 @@
 // This file exists to break the dependency loop
 // between Foundation and CoreFoundation.
+#include <stdint.h>
 #define GS_GC_STRONG
 #define GSNativeChar char
+#define UTF32Char uint32_t
--- config.h
+++ config.h
@@ -1,5 +1,11 @@
 #include <stdint.h>
-// TODO: add HAVE_ICU when Darling has it
+#define TZDIR "/usr/share/zoneinfo"
+#define HAVE_STDINT_H 1
+#define HAVE_ICU 1
 #define HAVE_LIBOBJC 1
 #define HAVE_LIBDISPATCH 1
+#define HAVE_UNICODE_UNORM_H 1
+#define HAVE_UNICODE_USTRING_H 1
+#define HAVE_UNICODE_UTRANS_H 1
+#define HAVE_UNICODE_UCNV_H 1
 
