5a22df59f962368ab4668fc254191bd7c6144e4c
Andrew Hyatt <ahyattdev@icloud.com>
2017-04-20 19:00:21 -0700

    Create proper include directory for CoreFoundation

--- NSZombie.h
+++ NSZombie.h
@@ -1,14 +1,1 @@
-#import <objc/runtime.h>
-#import <Foundation/NSObjCRuntime.h>
-
-#define ZOMBIE_PREFIX "_NSZombie_"
-
-extern uint8_t __CFZombieEnabled;
-extern uint8_t __CFDeallocateZombies;
-
-NS_ROOT_CLASS
-@interface _NSZombie_ {
-    Class isa;
-}
-
-@end
+include/CoreFoundation/./NSZombie.h
--- _CFUtilities.h
+++ _CFUtilities.h
@@ -1,10 +1,1 @@
-#include <CoreFoundation/CFString.h>
-#include <stdlib.h>
-
-static char *CFStringUTF8Copy(CFStringRef cfString) {
-    CFIndex length = CFStringGetLength(cfString);
-    CFIndex size = CFStringGetMaximumSizeForEncoding(length, kCFStringEncodingUTF8);
-    char *buffer = (char *)malloc(size);
-    CFStringGetCString(cfString, buffer, size, kCFStringEncodingUTF8);
-    return buffer;
-}
+include/CoreFoundation/./_CFUtilities.h
