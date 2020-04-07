3a53d4fa0d4e239d8d2f707ed951ff6ff1091f55
Ariel Abreu <facekapow@outlook.com>
2020-04-07 09:28:35 -0400

    Implement (at least partially) `_CFXPCCreateXPCObjectFromCFObject`

--- CFXPCBridge.c
+++ CFXPCBridge.c
@@ -22,6 +22,12 @@
 */
 
 #include <CoreFoundation/CFXPCBridge.h>
+#include <CoreFoundation/CFString.h>
+#include <CoreFoundation/CFDictionary.h>
+#include <CoreFoundation/CFNumber.h>
+#include <CoreFoundation/CFArray.h>
+#include <CoreFoundation/CFData.h>
+#include <CoreFoundation/CFNumber.h>
 
 CFTypeRef _CFXPCCreateCFObjectFromXPCObject(xpc_object_t xpcattrs)
 {
@@ -32,8 +38,114 @@ CFTypeRef _CFXPCCreateCFObjectFromXPCObject(xpc_object_t xpcattrs)
 	return NULL;
 }
 
-xpc_object_t _CFXPCCreateXPCObjectFromCFObject(CFTypeRef attrs)
-{
+static void CFXPCDictionaryApplier(const void* raw_key, const void* raw_value, void* context) {
+	struct {
+		char** keys;
+		xpc_object_t* objs;
+		size_t i;
+	}* ctx = context;
+
+	// NOTE(@facekapow):
+	// we're assuming that the dictionary passed in contains only CFStrings as keys.
+	// is this a bad assumption? probably maybe kinda sorta possibly definitely.
+	// however, xpc dictionaries can only contain strings as keys so...
+	//
+	// here's to hoping that whoever uses `_CFXPCCreateXPCObjectFromCFObject` knows this
+	// and only uses CFString keys for their dictionaries :/
+
+	CFStringRef key = raw_key;
+
+	CFIndex length = CFStringGetLength(key);
+	CFIndex maxSize = CFStringGetMaximumSizeForEncoding(length, kCFStringEncodingUTF8) + 1;
+	ctx->keys[ctx->i] = malloc(maxSize);
+	CFStringGetCString(key, ctx->keys[ctx->i], maxSize, kCFStringEncodingUTF8);
+
+	// we're also assuming that the values in the dictionary are all CF objects
+
+	ctx->objs[ctx->i] = _CFXPCCreateXPCObjectFromCFObject(raw_value);
+
+	++ctx->i;
+};
+
+xpc_object_t _CFXPCCreateXPCObjectFromCFObject(CFTypeRef attrs) {
+	CFTypeID id = CFGetTypeID(attrs);
+
+	if (id == CFStringGetTypeID()) {
+		CFStringRef str = attrs;
+		CFIndex length = CFStringGetLength(str);
+		CFIndex maxSize = CFStringGetMaximumSizeForEncoding(length, kCFStringEncodingUTF8) + 1;
+		char* tmp = malloc(maxSize);
+		if (!CFStringGetCString(str, tmp, maxSize, kCFStringEncodingUTF8)) {
+			free(tmp);
+			return NULL;
+		}
+		xpc_object_t xo = xpc_string_create(tmp);
+		free(tmp);
+		return xo;
+	}
+
+	if (id == CFDictionaryGetTypeID()) {
+		CFDictionaryRef dict = attrs;
+		CFIndex length = CFDictionaryGetCount(dict);
+		char** keys = malloc(sizeof(char*) * length);
+		xpc_object_t* objs = malloc(sizeof(xpc_object_t) * length);
+		struct {
+			char** keys;
+			xpc_object_t* objs;
+			size_t i;
+		} ctx = {
+			.keys = keys,
+			.objs = objs,
+			.i = 0,
+		};
+		CFDictionaryApplyFunction(dict, CFXPCDictionaryApplier, &ctx);
+
+		xpc_object_t xdict = xpc_dictionary_create(keys, objs, length);
+
+		for (CFIndex i = 0; i < length; ++i) {
+			free(keys[i]);
+			xpc_release(objs[i]);
+		}
+		free(keys);
+		free(objs);
+
+		return xdict;
+	}
+
+	if (id == CFBooleanGetTypeID()) {
+		CFBooleanRef boolean = attrs;
+		return xpc_bool_create(CFBooleanGetValue(boolean));
+	}
+
+	if (id == CFArrayGetTypeID()) {
+		CFArrayRef array = attrs;
+		CFIndex length = CFArrayGetCount(array);
+		xpc_object_t* objs = malloc(sizeof(xpc_object_t) * length);
+
+		for (CFIndex i = 0; i < length; ++i)
+			objs[i] = _CFXPCCreateXPCObjectFromCFObject(CFArrayGetValueAtIndex(array, i));
+
+		xpc_object_t xarray = xpc_array_create(objs, length);
+
+		for (CFIndex i = 0; i < length; ++i)
+			xpc_release(objs[i]);
+		free(objs);
+
+		return xarray;
+	}
+
+	if (id == CFDataGetTypeID()) {
+		CFDataRef data = attrs;
+		return xpc_data_create(CFDataGetBytePtr(data), CFDataGetLength(data));
+	}
+
+	if (id == CFNumberGetTypeID()) {
+		CFNumberRef num = attrs;
+		int64_t tmp = 0;
+		CFNumberGetValue(num, kCFNumberSInt64Type, &tmp);
+		return xpc_int64_create(tmp);
+	}
+
 	return NULL;
 }
 
