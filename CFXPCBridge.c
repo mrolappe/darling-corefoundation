0df921f1db7d60a4aa6d6fe144303ad047f884c5
Lubos Dolezel <lubos@dolezel.info>
2020-04-19 19:42:00 +0200

    Fix CFXPCBridge header, this file was never part of CoreBase

--- CFXPCBridge.c
+++ CFXPCBridge.c
@@ -1,24 +1,20 @@
-/* CFXPCBridge.c
+/*
+This file is part of Darling.
 
-   Copyright (C) 2016 Lubos Dolezel
+Copyright (C) 2016 Lubos Dolezel
 
-   This file is part of the GNUstep CoreBase Library.
+Darling is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-   This library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Lesser General Public
-   License as published by the Free Software Foundation; either
-   version 2.1 of the License, or (at your option) any later version.
+Darling is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-   This library is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Lesser General Public License for more details.
-   
-   You should have received a copy of the GNU Lesser General Public
-   License along with this library; see the file COPYING.LIB.
-   If not, see <http://www.gnu.org/licenses/> or write to the
-   Free Software Foundation, 51 Franklin Street, Fifth Floor,
-   Boston, MA 02110-1301, USA.
+You should have received a copy of the GNU General Public License
+along with Darling.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include <CoreFoundation/CFXPCBridge.h>
